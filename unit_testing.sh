79e014ae28582b03a64e1d7eccf3ee3cdc260b9c
Sven Jansen <sjansen@cellular.de>
2012-11-20 15:56:02 +0100

    added ocunit2junit support to enable Jenkins-like test results

--- unit_testing.sh
+++ unit_testing.sh
@@ -7,6 +7,9 @@ COVERAGEDIR=$(xcodebuild -scheme UnitTest -sdk $SIMULATORSDK -configuration Cove
 # kill Simulator
 sh ~/UnitTestScripts/kill_simulator.sh
 # run unit tests
+# OCUnit2JUnit Results
+xcodebuild -scheme UnitTest -sdk $SIMULATORSDK -configuration UnitTest TEST_AFTER_BUILD=YES ONLY_ACTIVE_ARCH=NO clean build 2>&1 | /Library/Ruby/Gems/1.8/gems/ocunit2junit-1.2/bin/ocunit2junit
+# custom HTML Results
 xcodebuild -scheme UnitTest -sdk $SIMULATORSDK -configuration UnitTest TEST_AFTER_BUILD=YES ONLY_ACTIVE_ARCH=NO clean build >$WORKSPACE/TestResults/UnitTesting.log
 # run test parser to render some beautiful HTML output
 sh ~/UnitTestScripts/unit_test_result_parser.sh $WORKSPACE/TestResults/UnitTesting.log $WORKSPACE/TestResults/UnitTesting.html $WORKSPACE/TestResults/IntermediateLog.log
